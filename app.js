ffd8e3613539fe51f6d85b2c24c1b4a12d25740d
“cmedinam” <cmedinam@co.ibm.com>
2024-08-05 15:10:31 -0500

    se utiliza pdfkit

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const controladorExcel = require('xlsx');
-const puppeteer = require('puppeteer')
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 const pug = require('pug');
+
+
+
 
 
 const processFiles = () => {
 
     //Info del archivo viene en req.file
     // const rutaArchivo = req.file.path
-    const rutaArchivo = '/Users/carlosmedina/Downloads/test.xlsx'
+    // const rutaArchivo = '/Users/carlosmedina/Downloads/test.xlsx'
+    const rutaArchivo = '/Users/carlosmedina/Downloads/filtrado.xlsx'
     //console.log(req.file)
     //Leer archivo
     const archivoExcel = controladorExcel.readFile(rutaArchivo)
     //leer hojas del archivo
     const hojasArchivo = archivoExcel.SheetNames;
-    console.log(hojasArchivo)
+    //console.log(hojasArchivo)
     //almacenar nombre primera hoja
     const nombreHoja = hojasArchivo[0]
     //guardar info en formato json
@@ -30,29 +36,51 @@ const processFiles = () => {
      const infoHoja =  processFiles();
      //console.log(infoHoja)
 
-const crearDocumento = async (nombre, propiedades) =>{
+     //buildPDF((data)=>{},(data)=>{})
 
-    // Cargar plantilla
-    const compiledFunction = pug.compileFile('./views/ticket.pug');
+// const crearDocumento = async (nombre, propiedades) =>{
 
-    // Enviar propiedades
-    const html = compiledFunction(propiedades);
-    // console.log(html)    
+//     // Cargar plantilla
+//     const compiledFunction = pug.compileFile('./views/ticket.pug');
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(html);
-    await page.pdf({path: `./documentos/${nombre}.pdf`, format: 'A4'});
-    await browser.close();
-}    
+//     // Enviar propiedades
+//     const html = compiledFunction(propiedades);
+//     // console.log(html)    
 
-infoHoja.forEach((ticket, index) => {
-    crearDocumento(ticket.TICKET, ticket)
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.setContent(html);
+//     await page.pdf({path: `./documentos/${nombre}.pdf`, format: 'A4'});
+//     await browser.close();
+// }    
+
+// infoHoja.forEach((ticket, index) => {
+//     crearDocumento(ticket.TICKET, ticket)
+// });
+
+
+function buildPDF(data){
+    const doc = new PDFDocument();
+
+    doc.fontSize(12).text(
+        `ticket: ${data.TICKET}      
+descripcion: ${data.DESCRIPCION}
+tipo: ${data.TIPO}
+grupo_resolutor: ${data.RESOLUTORGROUP}
+clasificacion_nivel_3: ${data.ID_CLASIFICACION3}
+cumple_fcr: ${data.CUMPLEFCR}
+detalle: ${data.DETALLE}
+solucion: ${data.SOLUCION}`
+)
+
+    doc.pipe(fs.createWriteStream(`./documentos/${data.TICKET}.pdf`))
+    doc.end();
+}
+
+
+infoHoja.forEach((ticket) => {
+    buildPDF(ticket)
 });
-
-
-
-
 
 
 
